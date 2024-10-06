a0e2eb0579caf722bd6217adb0e9ce5064e7e0b0
Irshad017 <161463145+irshad017@users.noreply.github.com>
2024-10-06 16:54:11 +0530

    Update_col chatRoom.tsx

--- frontend/src/compo/chatRoom.tsx
+++ frontend/src/compo/chatRoom.tsx
@@ -273,161 +273,159 @@ export const ChatRoom = () => {
 
 
         <div className={`flex flex-col h-screen ${isNightMode ? 'bg-gray-900 text-gray-200' : 'bg-gradient-to-r from-gray-800 via-gray-900 to-black text-white'}`}>
-            {/* Back Button */}
-            <div className="flex justify-start items-center p-2">
-                <button
-                    onClick={() => navigate('/blogs')}
-                    className={`p-2 ${isNightMode ? 'bg-gray-700' : 'bg-gray-300'} rounded-md hover:${isNightMode ? 'bg-gray-600 ' : 'bg-gray-400'} transition-all duration-300`}
-                >
-                    <FaArrowLeft className="text-lg" />
-                </button>
+    {/* Back Button */}
+    <div className="flex justify-start items-center p-2">
+        <button
+            onClick={() => navigate('/blogs')}
+            className={`p-2 ${isNightMode ? 'bg-gray-700' : 'bg-gray-300'} rounded-md hover:${isNightMode ? 'bg-gray-600 ' : 'bg-gray-400'} transition-all duration-300`}
+        >
+            <FaArrowLeft className="text-lg" />
+        </button>
 
-                {/* Toggle Night Mode Button */}
-                <button
-                    onClick={() => setIsNightMode(!isNightMode)}
-                    className={`h-10 p-2 rounded-full ml-auto ${isNightMode ? 'bg-gray-700 text-white' : 'bg-gray-300 text-gray-800 font-bold'} transition-all duration-300`}>
-                    {isNightMode ? 'Day Mode' : 'Night Mode'}
-                </button>
-            </div>
+        {/* Toggle Night Mode Button */}
+        <button 
+            onClick={() => setIsNightMode(!isNightMode)} 
+            className={`h-10 p-2 rounded-full ml-auto ${isNightMode ? 'bg-gray-700 text-white' : 'bg-gray-300 text-gray-800 font-bold'} transition-all duration-300`}>
+            {isNightMode ? 'Day Mode' : 'Night Mode'}
+        </button>
+    </div>
 
-            <div className="flex flex-col sm:flex-row w-full max-w-6xl mx-auto flex-grow gap-2 h-full">
-                {/* Users List */}
-                <div className={`sm:w-1/4 p-2 sm:p-4 rounded-xl shadow-2xl ${isNightMode ? 'bg-gray-800' : 'bg-gray-700'} sm:h-full`}>
-                    {/* Profile */}
-                    <div className="border-b border-gray-600 flex items-center p-2 mb-3">
-                        <div className="flex flex-col items-center sm:flex-row space-y-2 sm:space-y-0">
-                            <img
-                                src={url3}
-                                alt="User"
-                                className="w-10 h-10 rounded-full"
-                            />
-                            <div className="ml-4 text-center sm:text-left">
-                                {fetchUser ? (
-                                    <h2 className="text-sm font-semibold">Loading...</h2>
-                                ) : (
-                                    <>
-                                        <h2 className="text-sm font-semibold">{UserName?.split('@')[0]}</h2>
-                                        <p className="text-xs">{UserName}</p>
-                                    </>
-                                )}
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* Chat List */}
-                    <div>
-                        <input
-                            type="text"
-                            placeholder="Search..."
-                            className={`w-full text-sm sm:text-lg mb-4 py-2 px-3 border border-gray-600 ${isNightMode ? 'bg-gray-700 text-white' : 'bg-gray-200 text-black'} rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-500 transition-all duration-300`}
-                            value={searchTerm}
-                            onChange={(e) => setSearchTerm(e.target.value)}
-                        />
-                        <ul className="overflow-y-auto max-h-[70vh] sm:max-h-[50vh]">
-                            {fetched ? (
-                                <li className="text-center text-gray-500">Loading...</li>
-                            ) : (
-                                filteredUsers.map((user, idx) => (
-                                    <li
-                                        key={idx}
-                                        onClick={() => HandelSelectUserToChat({ username: user.username, ReceiverId: user.id })}
-                                        className={`border-b border-slate-200 dark:border-slate-700 flex items-center px-2 py-2 hover:${isNightMode ? 'bg-gray-600' : 'bg-gray-300'} rounded-lg cursor-pointer mb-2`}
-                                    >
-                                        <FaUser className="w-6 h-6 sm:w-8 sm:h-8 border-2 rounded-full bg-white text-black" />
-                                        <span className="ml-2 sm:ml-3 text-sm sm:text-base">{user.username.split('@')[0]}</span>
-                                    </li>
-                                ))
-                            )}
-                        </ul>
-                    </div>
-                </div>
-
-                {/* Messaging Section */}
-                <div className={`w-full sm:w-3/4 p-2 sm:p-4 rounded-xl flex flex-col ${isNightMode ? 'bg-gray-800' : 'bg-white'} h-full`}>
-                    {/* Chat Header */}
-                    <div className={`flex items-center justify-between border-b pb-2 mb-2 ${isNightMode ? 'border-gray-700' : 'border-gray-300'}`}>
-                        <div className="flex items-center">
-                            <img
-                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQlVYQsJxbEDr57v18Wmwi2rXLOGQui08vHw&s"
-                                alt="User" className="border-2 border-black w-8 h-8 rounded-full sm:w-10 sm:h-10" />
-                            <div className="ml-4">
-                                <h2 className="text-xs sm:text-sm font-semibold text-gray-300">{selectedUser?.split('@')[0] || "Select_User"}</h2>
-                                <p className="text-xs text-gray-500">{selectedUser || "Select_User"}</p>
-                            </div>
-                        </div>
-                        <button
-                            className="bg-gray-700 text-white rounded-full p-2 transition-all duration-300 hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50"
-                            aria-label="Refresh"
-                            onClick={HandleRefresh}
-                        >
-                            <FaSyncAlt className="text-white h-5 w-5 sm:h-6 sm:w-6 cursor-pointer" />
-                        </button>
-                    </div>
-
-                    {/* Messages Section with Scroll */}
-                    <div className="flex-grow overflow-y-auto">
-                        {PrevMsg.map((msg) => {
-                            const isOwnMessage = msg.senderId === parseInt(localStorage.getItem('UserId') || '0', 10);
-
-                            return (
-                                <div key={msg.id} className={`flex ${isOwnMessage ? 'justify-end' : ''} mb-4`}>
-                                    {!isOwnMessage && (
-                                        <img
-                                            src="https://via.placeholder.com/40"
-                                            alt="User"
-                                            className="w-6 h-6 sm:w-8 sm:h-8 rounded-full shadow-md mr-2"
-                                        />
-                                    )}
-                                    <div className={`max-w-[75%] md:max-w-sm rounded-t-xl p-2 sm:p-3 text-white shadow-lg ${isOwnMessage ? 'rounded-l-xl bg-gradient-to-r from-blue-600 to-blue-700' : 'rounded-r-xl bg-gradient-to-r from-gray-500 to-gray-600'}`}>
-                                        <p className="break-words text-xs sm:text-sm">{msg.content}</p>
-                                        <div className="flex items-center justify-between mt-1 sm:mt-2 text-xs sm:text-sm text-gray-300">
-                                            <span>
-                                                {typeof msg.timestamp === 'string'
-                                                    ? new Date(msg.timestamp).toISOString().slice(11, 16)
-                                                    : msg.timestamp.toISOString().slice(11, 16)}
-                                            </span>
-                                            {isOwnMessage && <FaTrash onClick={() => HandleDeleteMsg(msg.id)} className="text-red-500 cursor-pointer hover:text-red-700" />}
-                                        </div>
-                                    </div>
-                                    {isOwnMessage && (
-                                        <img
-                                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQlVYQsJxbEDr57v18Wmwi2rXLOGQui08vHw&s"
-                                            alt="User"
-                                            className="w-6 h-6 sm:w-8 sm:h-8 rounded-full shadow-md ml-2"
-                                        />
-                                    )}
-                                </div>
-                            );
-                        })}
-                    </div>
-
-                    {/* Input Box and Send Message */}
-                    <div className="mt-2 sm:mt-4 border-t pt-2 relative">
-                        <input
-                            value={newMsgPost}
-                            onChange={(e) => setNewMsgPost(e.target.value)}
-                            onKeyDown={(e) => {
-                                if (e.key === 'Enter') {
-                                    handleSendMessage();
-                                }
-                            }}
-                            type="text"
-                            placeholder="Type something..."
-                            className={`w-full p-2 pr-10 border border-gray-300 rounded-md ${isNightMode ? 'bg-gray-700 text-white' : 'bg-white text-black'}`}
-                        />
-                        <button
-                            onClick={handleSendMessage}
-                            className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-blue-500 text-white px-3 py-1 rounded-md hover:bg-blue-600 transition-all duration-300"
-                        >
-                            Send
-                        </button>
+    <div className="flex flex-col sm:flex-row w-full max-w-6xl mx-auto flex-grow gap-2 h-full">
+        {/* Users List */}
+        <div className={`sm:w-1/4 p-2 sm:p-4 rounded-xl shadow-2xl ${isNightMode ? 'bg-gray-800' : 'bg-gray-700'} sm:h-full`}>
+            {/* Profile */}
+            <div className="border-b border-gray-600 flex items-center p-2 mb-3">
+                <div className="flex flex-col items-center sm:flex-row space-y-2 sm:space-y-0">
+                    <img 
+                        src={url3}
+                        alt="User" 
+                        className="w-10 h-10 rounded-full"
+                    />
+                    <div className="ml-4 text-center sm:text-left">
+                        {fetchUser ? (
+                            <h2 className="text-sm font-semibold">Loading...</h2>
+                        ) : (
+                            <>
+                                <h2 className="text-sm font-semibold">{UserName?.split('@')[0]}</h2>
+                                <p className="text-xs">{UserName}</p>
+                            </>
+                        )}
                     </div>
                 </div>
             </div>
-            <Toaster />
+
+            {/* Chat List */}
+            <div>
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    className={`w-full text-sm sm:text-lg mb-4 py-2 px-3 border border-gray-600 ${isNightMode ? 'bg-gray-700 text-white' : 'bg-gray-200 text-black'} rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-500 transition-all duration-300`}
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <ul className="overflow-y-auto max-h-[70vh] sm:max-h-[50vh]">
+                    {fetched ? (
+                        <li className="text-center text-gray-500">Loading...</li>
+                    ) : (
+                        filteredUsers.map((user, idx) => (
+                            <li
+                                key={idx}
+                                onClick={() => HandelSelectUserToChat({ username: user.username, ReceiverId: user.id })}
+                                className={`border-b border-slate-200 dark:border-slate-700 flex items-center px-2 py-2 hover:${isNightMode ? 'bg-gray-600' : 'bg-gray-300'} rounded-lg cursor-pointer mb-2`}
+                            >
+                                <FaUser className="w-6 h-6 sm:w-8 sm:h-8 border-2 rounded-full bg-white text-black" />
+                                <span className="ml-2 sm:ml-3 text-sm sm:text-base">{user.username.split('@')[0]}</span>
+                            </li>
+                        ))
+                    )}
+                </ul>
+            </div>
         </div>
 
+        {/* Messaging Section */}
+        <div className={`w-full sm:w-3/4 p-2 sm:p-4 rounded-xl flex flex-col ${isNightMode ? 'bg-gray-800' : 'bg-white'} h-full`}>
+            {/* Chat Header */}
+            <div className={`flex items-center justify-between border-b pb-2 mb-2 ${isNightMode ? 'border-gray-700' : 'border-gray-300'}`}>
+                <div className="flex items-center">
+                    <img 
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQlVYQsJxbEDr57v18Wmwi2rXLOGQui08vHw&s"
+                        alt="User" className="border-2 border-black w-8 h-8 rounded-full sm:w-10 sm:h-10" />
+                    <div className="ml-4">
+                        <h2 className="text-xs sm:text-sm font-semibold text-gray-300">{selectedUser?.split('@')[0] || "Select_User"}</h2>
+                        <p className="text-xs text-gray-500">{selectedUser || "Select_User"}</p>
+                    </div>
+                </div>
+                <button 
+                    className="bg-gray-700 text-white rounded-full p-2 transition-all duration-300 hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50"
+                    aria-label="Refresh"
+                    onClick={HandleRefresh}
+                >
+                    <FaSyncAlt className="text-white h-5 w-5 sm:h-6 sm:w-6 cursor-pointer" />
+                </button>
+            </div>
 
+            {/* Messages Section with Scroll */}
+            <div className="flex-grow overflow-y-auto">
+                {PrevMsg.map((msg) => {
+                    const isOwnMessage = msg.senderId === parseInt(localStorage.getItem('UserId') || '0', 10);
+
+                    return (
+                        <div key={msg.id} className={`flex ${isOwnMessage ? 'justify-end' : ''} mb-4`}>
+                            {!isOwnMessage && (
+                                <img 
+                                    src="https://via.placeholder.com/40" 
+                                    alt="User" 
+                                    className="w-6 h-6 sm:w-8 sm:h-8 rounded-full shadow-md mr-2" 
+                                />
+                            )}
+                            <div className={`max-w-[75%] md:max-w-sm rounded-t-xl p-2 sm:p-3 text-white shadow-lg ${isOwnMessage ? 'rounded-l-xl bg-gradient-to-r from-blue-600 to-blue-700' : 'rounded-r-xl bg-gradient-to-r from-gray-500 to-gray-600'}`}>
+                                <p className="break-words text-xs sm:text-sm">{msg.content}</p>
+                                <div className="flex items-center justify-between mt-1 sm:mt-2 text-xs sm:text-sm text-gray-300">
+                                    <span>
+                                        {typeof msg.timestamp === 'string'
+                                            ? new Date(msg.timestamp).toISOString().slice(11, 16)
+                                            : msg.timestamp.toISOString().slice(11, 16)}
+                                    </span>
+                                    {isOwnMessage && <FaTrash onClick={() => HandleDeleteMsg(msg.id)} className="text-red-500 cursor-pointer hover:text-red-700" />}
+                                </div>
+                            </div>
+                            {isOwnMessage && (
+                                <img 
+                                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQlVYQsJxbEDr57v18Wmwi2rXLOGQui08vHw&s" 
+                                    alt="User" 
+                                    className="w-6 h-6 sm:w-8 sm:h-8 rounded-full shadow-md ml-2" 
+                                />
+                            )}
+                        </div>
+                    );
+                })}
+            </div>
+
+            {/* Input Box and Send Message */}
+            <div className="mt-2 sm:mt-4 border-t pt-2 relative">
+                <input
+                    value={newMsgPost}
+                    onChange={(e) => setNewMsgPost(e.target.value)}
+                    onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                            handleSendMessage();
+                        }
+                    }}
+                    type="text"
+                    placeholder="Type something..."
+                    className={`w-full p-2 pr-10 border border-gray-300 rounded-md ${isNightMode ? 'bg-gray-700 text-white' : 'bg-white text-black'}`}
+                />
+                <button 
+                    onClick={handleSendMessage}
+                    className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-blue-500 text-white px-3 py-1 rounded-md hover:bg-blue-600 transition-all duration-300"
+                >
+                    Send
+                </button>
+            </div>
+        </div>
+    </div>
+    <Toaster/>
+</div>
 
 
     );
