4b2c0c7c5b209ccb211d6dcd9dc3743a5348781f
Irshad017 <161463145+irshad017@users.noreply.github.com>
2024-10-06 11:34:24 +0530

    Updated URL config.ts

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,4 @@
 // CHANGE PORT ACCORDING TO YOUR BACKEND PORT RUNNING
 //  LIKE IF RUNNING AT 8787 THEN CHANGE TO 8787
-// export const BACKEND_URL = "https://back.irshadhussain7881.workers.dev"
-export const BACKEND_URL = "http://localhost:8787"
+export const BACKEND_URL = "https://back.irshadhussain7881.workers.dev"
+// export const BACKEND_URL = "http://localhost:8787"
